680699a2353127f4cc073d72bc99db07a9589eb2
CozyPenguin <coozypenguin@gmail.com>
2020-02-09 17:19:50 +0100

    fix bug from previous commit again

--- app/Main.hs
+++ app/Main.hs
@@ -9,9 +9,10 @@ main :: IO ()
 main = do
     args <- getArgs
 
-    case head args of
-        "" -> putStrLn "Penguor (c) 2020 Carl Schierig \n\n --help for help"
-        "--build" -> do
-            file <- readFile (args !! 1)
-            buildFromSource file
+    if null args
+        then putStrLn "Penguor (c) 2020 Carl Schierig \n\n --help for help"
+        else case head args of
+            "--build" -> do
+                file <- readFile (args !! 1)
+                buildFromSource file
 
